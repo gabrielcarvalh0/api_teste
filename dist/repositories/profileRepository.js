d4684f5fe210f868ac1346d3581c5669e6174dfa
Gabriel Carvalho <68152088+gabrielcarvalh0@users.noreply.github.com>
2023-01-14 10:51:15 -0300

    deleted

--- dist/repositories/profileRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.profileRepository = void 0;
-const data_source_1 = require("../data-source");
-const Profile_1 = require("../entities/Profile");
-// getrepositry pede a entity que eu desejo pegar
-exports.profileRepository = data_source_1.AppDataSource.getRepository(Profile_1.Profile);
--- dist/repositories/uploadRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadRepository = void 0;
-const data_source_1 = require("../data-source");
-const Upload_1 = require("../entities/Upload");
-// getrepositry pede a entity que eu desejo pegar
-exports.uploadRepository = data_source_1.AppDataSource.getRepository(Upload_1.Upload);
